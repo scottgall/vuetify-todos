b3710785e09ecb99a7b855fd3ff05bf0db24e903
Scott Gall <scottarthurgall@gmail.com>
2021-07-02 17:53:34 -0500

    added v-btn

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <h1>Homepage</h1> 
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore, maiores est natus minus nisi quo illum voluptatum, assumenda mollitia ex debitis voluptas exercitationem tenetur totam autem veniam! Repellendus, minus similique?</p>
+    <v-btn text color="pink">button</v-btn>
   </div>
 </template>
 
