e143a5466edfab9a30921b80d28f444d1d1666bd
Scott Gall <scottarthurgall@gmail.com>
2021-07-02 17:57:52 -0500

    working on icon

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,12 @@
   <div class="home">
     <h1>Homepage</h1> 
     <v-btn text color="pink">button</v-btn>
+
+    <v-btn class="pink white--text">
+      <span>email me</span>
+      <v-icon right large>email</v-icon>
+
+    </v-btn>
   </div>
 </template>
 
